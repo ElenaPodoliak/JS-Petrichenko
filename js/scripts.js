38406838fa67ea962a528a56573fac50babfaa0a
Elena <lena.yakushko@gmail.com>
2022-06-12 15:53:36 +0300

    start commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log(4 + +" obj");
+
+console.log(4 + + " 5");
+
+console.log(4 + " 5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++; //оператор инкремента - увеличение на единицу
+// decr--; //оператор декремента - уменьшение на единицу
+
+// console.log(incr);
+// console.log(decr);
+
+console.log(incr++);
+console.log(decr--);
