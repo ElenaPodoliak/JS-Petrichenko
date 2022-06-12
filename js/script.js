d060adda7243301b5c3ad9844cdcac985d4949e1
Elena <lena.yakushko@gmail.com>
2022-06-12 16:52:52 +0300

    first part of practice

--- js/script.js
+++ js/script.js
@@ -1,7 +1,33 @@
+
+'use strict';
+
+let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько оцените его", ""),
+      c = prompt("Один из последних просмотренных фильмов", ""),
+      d = prompt("На сколько оцените его", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;    
+
+
+console.log(personalMovieDB);
+
 /* Задание на урок:
 
 1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
 'Сколько фильмов вы уже посмотрели?'
+
 
 2) Создать объект personalMovieDB и в него поместить такие свойства:
     - count - сюда передается ответ на первый вопрос
@@ -21,5 +47,4 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
-'use strict';
 
